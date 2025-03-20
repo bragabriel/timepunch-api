e1ad579c0ab230821f604e95bc2df21b15b0dd42
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2025-03-20 19:56:12 -0300

    initial commit

---
+++ src/test/java/io/github/bragabriel/timepunch_api/TimepunchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bragabriel.timepunch_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimepunchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
