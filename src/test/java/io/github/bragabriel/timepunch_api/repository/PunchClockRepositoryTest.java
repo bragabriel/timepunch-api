c2a80e248375e25bf0bf620c15fac2fe470eb86a
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2025-03-23 19:19:18 -0300

    feat: add test profile configuration for unit tests

--- src/test/java/io/github/bragabriel/timepunch_api/repository/PunchClockRepositoryTest.java
+++ src/test/java/io/github/bragabriel/timepunch_api/repository/PunchClockRepositoryTest.java
@@ -10,9 +10,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PunchClockRepositoryTest {
 
 	@Autowired
