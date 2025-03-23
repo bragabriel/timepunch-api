286236e64d6aaac6be2f316979cfc3ab40a7f380
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2025-03-23 16:00:54 -0300

    feat: implement controller and service

---
+++ src/main/java/io/github/bragabriel/timepunch_api/application/controller/PunchClockController.java
@@ -0,0 +1,27 @@
+package io.github.bragabriel.timepunch_api.application.controller;
+
+import io.github.bragabriel.timepunch_api.application.service.PunchClockService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/punch-clock")
+@AllArgsConstructor
+@Tag(name = "Punch Clock", description = "Endpoints for punch clock management")
+public class PunchClockController {
+
+	private final PunchClockService service;
+
+	@PostMapping("/{userId}")
+	@Operation(summary = "Registers a work punch")
+	public ResponseEntity<Void> registerPunchClock(@PathVariable Long userId) {
+		service.registerPunchClock(userId);
+		return ResponseEntity.status(201).build();
+	}
+}
---
+++ src/main/java/io/github/bragabriel/timepunch_api/application/service/PunchClockService.java
@@ -0,0 +1,39 @@
+package io.github.bragabriel.timepunch_api.application.service;
+
+import io.github.bragabriel.timepunch_api.application.chain.PunchClockChainHandler;
+import io.github.bragabriel.timepunch_api.domain.entity.PunchClock;
+import io.github.bragabriel.timepunch_api.domain.entity.User;
+import io.github.bragabriel.timepunch_api.domain.repository.PunchClockRepository;
+import io.github.bragabriel.timepunch_api.domain.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class PunchClockService {
+
+	private final PunchClockRepository punchClockRepository;
+	private final UserRepository userRepository;
+	private final PunchClockChainHandler firstHandler;
+
+	@Transactional
+	public void registerPunchClock(final Long userId) {
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+		LocalDateTime registerTime = LocalDateTime.now();
+
+		List<PunchClock> userPunches = punchClockRepository.findByUserIdAndDate(userId, registerTime.toLocalDate());
+
+		firstHandler.handle(user, userPunches, registerTime);
+
+		PunchClock punchClock = new PunchClock();
+		punchClock.setUser(user);
+		punchClock.setTimestamp(registerTime);
+		punchClockRepository.save(punchClock);
+	}
+}
