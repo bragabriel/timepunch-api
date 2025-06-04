8b27669e087fee8e9077e0e73b43a7b092ea2874
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2025-06-03 21:44:44 -0300

    feat: modify response

--- src/main/java/io/github/bragabriel/timepunch_api/application/dto/PunchClockResponse.java
+++ src/main/java/io/github/bragabriel/timepunch_api/application/dto/PunchClockResponse.java
@@ -3,6 +3,6 @@ package io.github.bragabriel.timepunch_api.application.dto;
 import java.time.LocalDateTime;
 
 public record PunchClockResponse(
-		Long id, String name, LocalDateTime timePunch
+		String name, LocalDateTime timePunch
 ) {
 }
